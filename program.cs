e1ff76fea0298472255432978a44ee47478114fb
lahion30-eng <lahion30@gmail.com>
2026-03-21 18:38:38 -0400

    Update program.cs

--- program.cs
+++ program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hola desde rama1");
+        Console.WriteLine("Ingrese el nombre del cliente:");
+        string nombre = Console.ReadLine();
+
+        Console.WriteLine("Ingrese la edad del cliente:");
+        int edad = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Cliente: " + nombre);
+        Console.WriteLine("Edad: " + edad);
     }
 }
